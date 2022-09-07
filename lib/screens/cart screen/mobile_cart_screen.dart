d32c9bb88b5ded07592275593ad613c33c5a012a
Tirth88663 <tirthp88663@gmail.com>
2022-09-07 23:00:00 +0530

    added horizontal scrollable list in homepage

--- lib/screens/cart screen/mobile_cart_screen.dart
+++ lib/screens/cart screen/mobile_cart_screen.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MobileCartScreen extends StatelessWidget {
   const MobileCartScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Text(
+        "Cart Screen",
+        style: GoogleFonts.poppins(),
+      ),
+    );
   }
 }
