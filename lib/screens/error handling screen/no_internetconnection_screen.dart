1f734e827379f73c7c9bd751a6ffeb55f2e76ea2
Tarun Kanti Jena <98694852+tkj2410@users.noreply.github.com>
2022-09-25 01:09:47 +0530

    Update and rename no_internet_page.dart to no_internetconnection_screen.dart

--- lib/screens/error handling screen/no_internetconnection_screen.dart
+++ lib/screens/error handling screen/no_internetconnection_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-class NoInternetConnectionpage extends StatefulWidget {
+class NoInternetConnectionpage extends StatefulWidget
+{
   const NoInternetConnectionpage({Key? key}) : super(key: key);
 
   @override
@@ -10,10 +11,11 @@ class NoInternetConnectionpage extends StatefulWidget {
 class _NoInternetConnectionpagestate extends State<NoInternetConnectionpage> {
   @override
   Widget build(BuildContext context) {
-    var height = MediaQuery.of(context).size.height;
-    var width = MediaQuery.of(context).size.width;
+    //var height = MediaQuery.of(context).size.height;
+    //var width = MediaQuery.of(context).size.width;
+    
     return  Scaffold(
-      //backgroundColor: const Color(0xFFE5413F),
+     
       backgroundColor: Colors.white,
       body: Center(
         child: Stack(
@@ -26,9 +28,10 @@ class _NoInternetConnectionpagestate extends State<NoInternetConnectionpage> {
               color: Colors.white.withOpacity(0.4),
               colorBlendMode: BlendMode.modulate,
             ),*/
+            
             Padding(
               padding: EdgeInsets.only(bottom: 260.0),
-              child: Image.asset("assets/no_internet.PNG",
+              child: Image.asset("assets/images/no_internet.PNG",
 
                 fit: BoxFit.cover,
                 height: 300.0,
@@ -36,6 +39,7 @@ class _NoInternetConnectionpagestate extends State<NoInternetConnectionpage> {
                 //color: Colors.transparent,
                 colorBlendMode: BlendMode.modulate,),
             ),
+            
             Padding(
               padding: const EdgeInsets.only(top: 150.0),
               child: Text('WHOOPS !',
@@ -48,6 +52,7 @@ class _NoInternetConnectionpagestate extends State<NoInternetConnectionpage> {
                 ),
               ),
             ),
+            
             Padding(
               padding: const EdgeInsets.only(top: 300.0),
               child: Text('Slow or No Internet Connection\nCheck and Try Again',
@@ -60,29 +65,6 @@ class _NoInternetConnectionpagestate extends State<NoInternetConnectionpage> {
                 ),
               ),
             ),
-
-            /*Padding(
-              padding: const EdgeInsets.only(top: 165.0),
-              child: const SizedBox(
-                width: 170.0,
-                height: 33.5,
-
-                child: DecoratedBox(
-                  decoration: const BoxDecoration(
-                    color: Colors.white, borderRadius: BorderRadius.all(Radius.circular(20.0),
-
-                  ),),
-
-                  child:Text('Thank You',
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(
-                        fontSize:28.0,
-                        color: Colors.black, fontWeight: FontWeight.w600),
-
-                  ) ,
-                ),
-              ),
-            ),*/
 
           ],
         ),
